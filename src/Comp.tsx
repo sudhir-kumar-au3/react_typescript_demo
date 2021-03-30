0f8a3c9c9dc151b9a9cce0d542adf823b0a7e211
Sudhir Kumar <52371851+sudhir-kumar-au3@users.noreply.github.com>
2021-03-30 08:38:15 +0530

    react typescript demo project

---
+++ src/Comp.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+const Comp: FC = () => {
+    return (
+        <div>
+             <p>My name is <h1>sudhir</h1> kumar.</p>
+        </div>
+    )
+}
+
+export default Comp
